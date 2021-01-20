4d35a8b8100e167d1a2c024a4f86c6169c8e8190
maheshdivakaran01 <mahesh.divakaran01@gmail.com>
2021-01-20 15:03:13 +0000

    Updated

---
+++ Day 3 Objects.R
@@ -0,0 +1,22 @@
+# ls() use to list all the objects in R
+ls()
+
+a = 85
+
+ls()
+
+b = "Hi All!"
+
+vishnu = "Vishnu Sagar"
+
+ls()
+
+# To remove an object
+rm(a)
+ls()
+
+
+rm(list = ls())
+ls()
+
+
