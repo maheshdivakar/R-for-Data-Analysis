352352f6ed31901aec0764320d8c31aeeb25065a
maheshdivakaran01 <mahesh.divakaran01@gmail.com>
2021-01-20 14:53:18 +0000

    Updated

--- Day 3.R
+++ Day 3.R
@@ -1,3 +1,49 @@
 print("Welcome to Day 3!!!")
 
+# R as a calculator
+
+# Addition
+55 + 63
+#Subtraction
+69-89
+#Multiplication
+96 * 56
+#Division
+896 / 3
+
+#trigonometry
+sin(96)
+
+cos(562)
+
+cosh(2.3)
+
+log(20)
+
+exp(56)
+
+#Powers
+
+95^2
+
+95^3
+
+pi
+
+
+ln(63)
+
+
+log(100)
+
+
+?log
+??log
+
+log10(250)
+
+#Reminders
+50 %% 7
+
+
 
